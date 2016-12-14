8b9e9586822c0d423d61c178f28ff6073815bbac
martarim <martarim@users.noreply.github.com>
2016-12-14 13:49:57 +0200

    Created by Coding Ground Client

---
+++ New Project-20161214/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int z;  
+
+z = 13;
+
+int r;
+r = 2016;
+
+int f;
+f = 7;
+
+cout << " Apsveicu tevi tavos " << z << " Ziemassvetkos " << endl;
+cout << " Un sveicieni jaunaja " << r << ". gada" << endl;
+cout << "   *   " <<   endl;
+cout << "  ***  " <<  endl;
+cout << " ***** " << endl;
+cout << "   *   " <<    endl;
+cout << " Ar milestibu " << endl;
+cout << "       Marta Rimsa " << f << ". klase" << endl;
+ 
+
+
+   return 0;
+}
