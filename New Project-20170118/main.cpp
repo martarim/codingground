ef1cae0711690438537b5bf9fd9d442d772824b4
martarim <martarim@users.noreply.github.com>
2017-01-18 13:48:06 +0200

    Created by Coding Ground Client

---
+++ New Project-20170118/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int mod;
+    int y;
+    int summa;
+    double dala;
+    int reiz;
+    int atnem;
+    int v;
+    int z;
+    int rezviens;
+    int rezdivi;
+    int reztris;
+    
+    z = 2;
+    v = 7;
+    y = 4;
+    x = 6;
+    
+    mod = x % y;
+    summa = x + y;
+    reiz = x * y; 
+    dala = x /  (double) y;
+    atnem = x - y;
+    
+    cout << " rezviens rezultats " << rezviens << endl;
+    cout << " rezdivi ir " << rezdivi << endl ;
+    cout << " reztris ir " << reztris << endl ;
+  
+    cout << " Skaitla " << x << " un " << y << " atlikums ir " << mod << endl;
+    cout << " Skaitla " << x << " un " << y << " summa ir " << summa << endl;
+    cout << " Skaitla " << x << " un " << y << " starpiba ir " << atnem << endl;
+    cout << " Skaitla " << x << " un " << y << " dalijums ir " << dala << endl;
+    cout << " Skaitla " << x << " un " << y << " reizinajums ir " << reiz << endl;
+ 
+ 
+   
+    
+   cout << " . " << endl;
+   cout << " Es esmu Marta Rimsa" << endl; 
+   cout << " es eju " << v << ". klase " << endl;
+   cout << "  Un si ir programmesanas stunda " << endl;
+   cout << ".. " << endl;
+   
+   return 0;
+}
+
