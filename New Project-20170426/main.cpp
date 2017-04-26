909b072a5ad8705f2a9217ae8680ea068199fce0
martarim <martarim@users.noreply.github.com>
2017-04-26 13:43:40 +0300

    Created by Coding Ground Client

---
+++ New Project-20170426/main.cpp
@@ -0,0 +1,19 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+    
+int y;
+     
+ cout << "ievadi skaitli" << endl;
+ cin >> y;
+ 
+ for (int x = 1 ; x <= y ; x++)
+ cout << "est" << endl;
+ 
+}
+
+    return 0;
+}
